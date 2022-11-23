554e5d70189fb1f8a92269df68764c6c0a269db2
evance-14 <kiropevans097@gmail.com>
2022-11-23 22:59:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+console.log(saturdayFun("watch ball"));
+function mondayWork(work = "go to the office"){
+    return `This Monday, I will ${work}.`
+}
+console.log(mondayWork("go to the field"));
+function wrapAdjective(visual = "*"){
+    return function(adjective = "special"){
+        return `You are ${visual}${adjective}${visual}!`;
+    };
+}
+console.log(wrapAdjective("||")("a dedicated programmer"));
+
+
